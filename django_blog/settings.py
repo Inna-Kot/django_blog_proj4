1ec26de52db16863b508a55146e3e5182318dc29
Inna-Kot <kotkovets.dev@outlook.ie>
2025-11-25 22:57:03 +0000

    Prepare Django project for Render deployment: Add WhiteNoise, DB config, build.sh, update requirements

--- django_blog/settings.py
+++ django_blog/settings.py
@@ -122,3 +122,14 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+import os
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+STATIC_URL = '/static/'
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
