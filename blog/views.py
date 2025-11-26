fab7c5360a698bbaeaa26b22da3bb5a400d018e2
Inna-Kot <kotkovets.dev@outlook.ie>
2025-11-26 22:55:57 +0000

    Update my_blog view to render home.html template

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,5 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
 def my_blog(request):
-    return HttpResponse("Hello, blog!")
+    return render(request, 'blog/home.html')
