8e3945db8b5c39375bedf73273c1b66fef54b871
Inna-Kot <kotkovets.dev@outlook.ie>
2025-12-03 22:02:29 +0000

    Add create_superuser view for temporary superuser creation

--- blog/views.py
+++ blog/views.py
@@ -3,3 +3,12 @@ from django.shortcuts import render
 # Create your views here.
 def my_blog(request):
     return render(request, 'blog/home.html')
+
+from django.contrib.auth.models import User
+from django.http import HttpResponse
+
+def create_superuser(request):
+    if not User.objects.filter(username='admin').exists():
+        User.objects.create_superuser('admin', 'admin@example.com', 'YourStrongPassword123')
+        return HttpResponse("Superuser created!")
+    return HttpResponse("Superuser already exists!")
