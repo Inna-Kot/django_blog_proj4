b2c3e4ee09b10c6338292b39f89d69a43125d047
Inna-Kot <kotkovets.dev@outlook.ie>
2026-01-18 13:46:02 +0000

    Update settings.py to use SECRET_KEY and DATABASE_URL from env.py

--- django_blog/settings.py
+++ django_blog/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-vi!tv!agdfv1e8irb$8c-4$3&h2c^o$%jblp%hsh-o-ljm+)d8'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
