84d077f4922383abf648fcb533c80b82c646f173
Inna-Kot <kotkovets.dev@outlook.ie>
2026-01-19 09:42:42 +0000

    added comments section and comment form to post_detail view and template

--- blog/views.py
+++ blog/views.py
@@ -1,18 +1,37 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import Post
+from .forms import CommentForm  
 
 class PostList(generic.ListView):
-    queryset = Post.objects.filter(status=1)  
-    template_name = "blog/index.html"       
-    paginate_by = 6  
+    queryset = Post.objects.filter(status=1)
+    template_name = "blog/index.html"
+    paginate_by = 6
 
 def post_detail(request, slug):
     queryset = Post.objects.filter(status=1)
     post = get_object_or_404(queryset, slug=slug)
 
+    comments = post.comments.filter(active=True)
+    new_comment = None
+
+    if request.method == 'POST':
+        comment_form = CommentForm(request.POST)
+        if comment_form.is_valid():
+            new_comment = comment_form.save(commit=False)
+            new_comment.post = post
+            new_comment.comment_author = request.user
+            new_comment.save()
+    else:
+        comment_form = CommentForm()
+
     return render(
         request,
         "blog/post_detail.html",
-        {"post": post},
+        {
+            "post": post,
+            "comments": comments,
+            "new_comment": new_comment,
+            "comment_form": comment_form
+        },
     )
