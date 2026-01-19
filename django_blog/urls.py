f200baffbbfdad179b8f0b05a5f514f5bb7727d8
Inna-Kot <kotkovets.dev@outlook.ie>
2026-01-19 00:20:58 +0000

    Fix ImportError: remove old my_blog import and use include for blog.urls

--- django_blog/urls.py
+++ django_blog/urls.py
@@ -15,12 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from blog.views import my_blog
+from django.urls import path, include
 
 urlpatterns = [
-    path('', my_blog, name='home'),  # домашня сторінка на корені
-    path('admin/', admin.site.urls),  # адмінка
+    path('', include('blog.urls')),
+    path('admin/', admin.site.urls),
 ]
-
-
