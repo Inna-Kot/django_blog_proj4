586bfd95099d92fa67f038fb6fb1f06da7f584b2
Inna-Kot <kotkovets.dev@outlook.ie>
2026-01-19 00:41:48 +0000

    Refactor admin: improved Post admin with list display, search, filter, and slug auto-fill

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
-from .models import Post, Comment
+from .models import Post
 
-admin.site.register(Post)
-admin.site.register(Comment)
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'status', 'created_on')
+    search_fields = ['title', 'content']
+    list_filter = ('status', 'created_on')
+    prepopulated_fields = {'slug': ('title',)}
