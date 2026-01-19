27c73e726871b3d3899d8d65bc7986b206a910eb
Inna-Kot <kotkovets.dev@outlook.ie>
2026-01-19 10:29:42 +0000

    Added About view, template and URLs

--- about/views.py
+++ about/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from .models import About
 
-# Create your views here.
+def about_view(request):
+    about = About.objects.order_by('-updated_on').first()  # останній запис
+    return render(request, "about/about.html", {"about": about})
