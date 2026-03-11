de00bbf1c1fe950706dc25dbad1253d278b51485
Inna-Kot <kotkovets.dev@outlook.ie>
2026-03-11 23:12:10 +0000

    Configure Summernote for About model and update README

--- about/admin.py
+++ about/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from .models import About
+from django_summernote.admin import SummernoteModelAdmin
 
 @admin.register(About)
-class AboutAdmin(admin.ModelAdmin):
+class AboutAdmin(SummernoteModelAdmin):
     list_display = ('title', 'updated_on')
+    summernote_fields = ('content',)
