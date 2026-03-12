a65c690e0070f9919cbc9a0e1e0f7252b6df5f4e
Inna-Kot <kotkovets.dev@outlook.ie>
2026-03-12 22:12:29 +0000

    Create CommentForm with custom textarea widget

--- blog/forms.py
+++ blog/forms.py
@@ -2,9 +2,12 @@ from django import forms
 from .models import Comment
 
 class CommentForm(forms.ModelForm):
+    """
+    Form class for users to post comments on blog posts.
+    """
     class Meta:
         model = Comment
-        fields = ['body']  # тільки текст коментаря
+        fields = ['body']  # Include only the 'body' field in the form
         widgets = {
             'body': forms.Textarea(attrs={'rows': 3, 'placeholder': 'Write a comment...'})
         }
