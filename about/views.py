bb24b74bba85b9e0c22d9cfe4b5b1dca9b774c28
Inna-Kot <kotkovets.dev@outlook.ie>
2026-03-13 16:03:05 +0000

    Add logic to handle newsletter subscription in about_me view

--- about/views.py
+++ about/views.py
@@ -1,6 +1,29 @@
 from django.shortcuts import render
+from django.contrib import messages
 from .models import About
+from .forms import NewsletterForm
 
-def about_view(request):
-    about = About.objects.order_by('-updated_on').first()  # останній запис
-    return render(request, "about/about.html", {"about": about})
+def about_me(request):
+    """
+    Renders the About page and handles newsletter subscriptions.
+    """
+    if request.method == "POST":
+        newsletter_form = NewsletterForm(data=request.POST)
+        if newsletter_form.is_valid():
+            newsletter_form.save()
+            messages.add_message(
+                request, messages.SUCCESS,
+                "Success! You are now subscribed to our cozy newsletter."
+            )
+
+    about = About.objects.all().order_by('-updated_on').first()
+    newsletter_form = NewsletterForm()
+
+    return render(
+        request,
+        "about/about.html",
+        {
+            "about": about,
+            "newsletter_form": newsletter_form
+        },
+    )
