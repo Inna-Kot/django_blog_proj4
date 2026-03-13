eade524c0b987cb410846215ea2b328a893b426e
Inna-Kot <kotkovets.dev@outlook.ie>
2026-03-13 15:55:39 +0000

    Register NewsletterSubscription in admin panel with search and filter

--- about/admin.py
+++ about/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
-from .models import About
-from django_summernote.admin import SummernoteModelAdmin
+from .models import NewsletterSubscription
 
-@admin.register(About)
-class AboutAdmin(SummernoteModelAdmin):
-    list_display = ('title', 'updated_on')
-    summernote_fields = ('content',)
+@admin.register(NewsletterSubscription)
+class NewsletterSubscriptionAdmin(admin.ModelAdmin):
+    """
+    Custom admin configuration for the NewsletterSubscription model.
+    """
+    list_display = ('name', 'email', 'subscribed_on')
+    search_fields = ('name', 'email')
+    list_filter = ('subscribed_on',)
