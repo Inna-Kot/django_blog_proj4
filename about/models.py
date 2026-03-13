090052f3c60cba91b3a41a084bf6a2c3a4e75336
Inna-Kot <kotkovets.dev@outlook.ie>
2026-03-13 15:41:39 +0000

    Implement NewsletterSubscription as a unique model and apply migrations

--- about/models.py
+++ about/models.py
@@ -7,3 +7,14 @@ class About(models.Model):
 
     def __str__(self):
         return self.title
+
+class NewsletterSubscription(models.Model):
+    """
+    Model for storing unique newsletter subscriptions.
+    """
+    name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    subscribed_on = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.name} - {self.email}"
