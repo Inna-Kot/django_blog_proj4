bcad89375d8243736f0b676b940dc79ec66b3bde
Inna-Kot <kotkovets.dev@outlook.ie>
2026-03-13 11:06:36 +0000

    Feature: add success messages and refine comment visibility logic

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,4 @@
+from django.contrib import messages # Import Django messages framework
 from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import Post
@@ -12,7 +13,8 @@ def post_detail(request, slug):
     queryset = Post.objects.filter(status=1)
     post = get_object_or_404(queryset, slug=slug)
 
-    comments = post.comments.filter(approved=True)
+    comments = post.comments.all().order_by("-created_on")
+    comment_count = post.comments.filter(approved=True).count()
     new_comment = None
 
     if request.method == 'POST':
@@ -22,6 +24,10 @@ def post_detail(request, slug):
             new_comment.post = post
             new_comment.author = request.user
             new_comment.save()
+            messages.add_message(
+                request, messages.SUCCESS,
+                'Comment submitted and awaiting approval'
+            )
     else:
         comment_form = CommentForm()
 
@@ -32,6 +38,7 @@ def post_detail(request, slug):
             "post": post,
             "comments": comments,
             "new_comment": new_comment,
+            "comment_count": comment_count,
             "comment_form": comment_form
         },
     )
